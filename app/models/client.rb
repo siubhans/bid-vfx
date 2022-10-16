2ddf40ca24368a23c0fea45648d629342e3c9484
siubhans <103741645+siubhansm@users.noreply.github.com>
2022-10-16 16:36:56 +0100

    mvc config

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,3 +1,7 @@
 class Client < ApplicationRecord
+  has_many :bids, dependent: :destroy
+
   belongs_to :user
+
+  validates :name, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-  resources :bids
-  resources :clients
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  namespace :api do
+    namespace :v1 do
+      resources :bids
+      resources :clients
+    end
+  end
+
+  root to: "home#index"
 end
