437a6850174e12b511f6a1223cfe9eac636a3316
siubhans <103741645+siubhansm@users.noreply.github.com>
2022-12-04 18:58:28 +0000

    bid costs

--- front/src/components/views/bid-view.vue
+++ front/src/components/views/bid-view.vue
@@ -116,6 +116,7 @@ export default {
       onOff: "on",
       editing: false,
       currentUser: [],
+      shots: [],
     };
   },
   created() {
@@ -145,6 +146,7 @@ export default {
         .then(() => {
           this.editing = false;
           this.printList();
+          this.getShots(bid, this.bids[index].cost);
         })
         .catch((error) => console.log(error, "Cannot update record"));
     },
@@ -173,6 +175,33 @@ export default {
           this.currentUser = response.data;
         })
         .catch((error) => console.log(error, "Cannot get user"));
+    },
+    getShots(bid, rate) {
+      this.plain
+        .get("/shots/", {
+          params: {
+            bid_id: bid,
+          },
+        })
+        .then((response) => {
+          this.shots = response.data;
+          this.updateCosts(this.shots, rate);
+        });
+    },
+    updateCosts(shots, rate) {
+      const shotArray = Array.from(shots);
+      shotArray.forEach((shot) => {
+        this.secured
+          .patch(`shots/${shot.id}`, {
+            shot: {
+              total: rate * shot.days,
+            },
+          })
+          .then(() => {
+            console.log("updated shot", shot.id);
+          })
+          .catch((error) => console.log(error, "Cannot update shot"));
+      });
     },
     formatCurrency(cost) {
       let val = (cost / 1).toFixed(2);
