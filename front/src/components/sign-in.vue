df0a6908bb6562ad2f84aedda1231dd65ffc4cda
siubhans <103741645+siubhansm@users.noreply.github.com>
2022-12-08 19:26:38 +0000

    favicon

--- front/src/components/sign-in.vue
+++ front/src/components/sign-in.vue
@@ -110,14 +110,10 @@ export default {
 };
 </script>
 <style scoped>
-/* #app {
-  background: url("../assets/stars.png") no-repeat center center fixed;
-  -webkit-background-size: cover;
-  -moz-background-size: cover;
-  -o-background-size: cover;
-  background-size: cover;
-  height: 100vh;
-} */
+body {
+  padding-left: 0;
+}
+
 .imgBG {
   position: fixed;
   left: 0;
