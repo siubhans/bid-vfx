ef6b04ef81f7a3c0fa6b94d8bfb816792d903637
siubhans <103741645+siubhansm@users.noreply.github.com>
2022-12-11 16:06:59 +0000

    new bid and client FE form validation

--- front/src/components/new/new-bid.vue
+++ front/src/components/new/new-bid.vue
@@ -4,26 +4,47 @@
     <div v-if="!bidId" class="innerContainer">
       <mainHeading title="New Bid" />
       <form @submit.prevent="addNewBid">
-        <div v-if="error">{{ error }}</div>
         <div class="mb-3">
           <label for="project" class="form-label">Project name</label>
           <input type="text" class="form-control" v-model="project" id="name" />
+          <p
+            v-for="error of v$.project.$errors"
+            :key="error.$uid"
+            class="error"
+          >
+            <strong>{{ error.$message }}</strong>
+          </p>
         </div>
+
         <div class="mb-3 row">
-          <div class="col">
+          <div class="col col-1">
             <label for="fps" class="form-label">FPS</label>
             <input type="number" class="form-control" v-model="fps" id="fps" />
+            <p v-for="error of v$.fps.$errors" :key="error.$uid" class="error">
+              <strong>{{ error.$message }}</strong>
+            </p>
           </div>
-          <div class="col">
+          <div class="col col-2">
             <label for="resolution" class="form-label">Resolution</label>
-            <input
-              type="text"
-              class="form-control"
+            <select
+              class="form-select"
+              aria-label="Default select example"
               v-model="resolution"
               id="resolution"
-            />
+            >
+              <option v-for="res in resolutions" :key="res">
+                {{ res }}
+              </option>
+            </select>
+            <p
+              v-for="error of v$.resolution.$errors"
+              :key="error.$uid"
+              class="error"
+            >
+              <strong>{{ error.$message }}</strong>
+            </p>
           </div>
-          <div class="col">
+          <div class="col col-1">
             <label for="cost" class="form-label">Day rate</label>
             <input
               type="number"
@@ -31,6 +52,9 @@
               v-model="cost"
               id="cost"
             />
+            <p v-for="error of v$.cost.$errors" :key="error.$uid" class="error">
+              <strong>{{ error.$message }}</strong>
+            </p>
           </div>
         </div>
         <div v-if="clients.length">
@@ -48,6 +72,13 @@
               {{ client.name }} : {{ client.studio }}
             </option>
           </select>
+          <p
+            v-for="error of v$.clientId.$errors"
+            :key="error.$uid"
+            class="error"
+          >
+            <strong>{{ error.$message }}</strong>
+          </p>
           <button type="submit" class="btn btn-secondary">Create</button>
         </div>
         <div v-else>
@@ -61,6 +92,15 @@
 </template>
 
 <script>
+import useValidate from "@vuelidate/core";
+import {
+  required,
+  minLength,
+  maxLength,
+  between,
+  numeric,
+} from "@vuelidate/validators";
+
 import sideBar from "@/components/ui/side-bar.vue";
 import mainHeading from "@/components/ui/main-heading.vue";
 
@@ -71,36 +111,65 @@ export default {
   },
   data() {
     return {
+      v$: useValidate(),
       project: "",
       fps: "",
       resolution: "",
       clientId: "",
+      cost: "",
       bidId: "",
       user: {},
       clients: {},
+      error: [],
+      resolutions: [
+        "2k HD (1920 × 1080)",
+        "2K (2048 x 1080)",
+        "4K (4096 x 2160)",
+        "4K UHD (3840 × 2160)",
+        "5K (5120 × 2700)",
+        "8K UHD (7680 × 4320)",
+        "720p (1280 × 720)",
+        "576p (720 × 576)",
+        "480p (720 × 480)",
+      ],
     };
   },
   created() {
     this.getClientList();
   },
+  validations() {
+    return {
+      project: { required, minLength: minLength(4), maxLength: maxLength(20) },
+      fps: { required, betweenValue: between(10, 100) },
+      resolution: { required },
+      cost: { required, numeric, betweenValue: between(0, 100000000000) },
+      clientId: { required },
+    };
+  },
   methods: {
     addNewBid() {
-      this.secured
-        .post("/bids", {
-          bid: {
-            project: this.project,
-            fps: this.fps,
-            resolution: this.resolution,
-            cost: this.cost,
-            client_id: this.clientId,
-          },
-        })
-        .then((response) => {
-          this.bidId = response.data.id;
-          localStorage.currentBid = this.bidId;
-          this.$router.replace("/buildBid");
-        })
-        .catch((error) => console.log(error, "Cannot create bid"));
+      this.v$.$validate();
+      if (!this.v$.$error) {
+        this.secured
+          .post("/bids", {
+            bid: {
+              project: this.project,
+              fps: this.fps,
+              resolution: this.resolution,
+              cost: this.cost,
+              client_id: this.clientId,
+            },
+          })
+          .then((response) => {
+            this.bidId = response.data.id;
+            localStorage.currentBid = this.bidId;
+            this.$router.replace("/buildBid");
+            this.error = [];
+          })
+          .catch((error) => console.log(error, "Cannot create bid"));
+      } else {
+        console.log("Form is invalid");
+      }
     },
     getClientList() {
       this.secured.get("/clients").then((response) => {
@@ -117,4 +186,11 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.col-1 {
+  flex: 1;
+}
+.col-2 {
+  flex: 2;
+}
+</style>
--- front/src/components/new/new-client.vue
+++ front/src/components/new/new-client.vue
@@ -15,6 +15,9 @@
             id="name"
             placeholder="Client's Name"
           />
+          <p v-for="error of v$.name.$errors" :key="error.$uid" class="error">
+            <strong>{{ error.$message }}</strong>
+          </p>
         </div>
         <div class="mb-3">
           <label for="producer" class="form-label">Producer</label>
@@ -25,6 +28,13 @@
             id="producer"
             placeholder="Producer's Name"
           />
+          <p
+            v-for="error of v$.producer.$errors"
+            :key="error.$uid"
+            class="error"
+          >
+            <strong>{{ error.$message }}</strong>
+          </p>
         </div>
         <div class="mb-3">
           <label for="studio" class="form-label">Studio</label>
@@ -35,6 +45,9 @@
             id="studio"
             placeholder="Studio Name"
           />
+          <p v-for="error of v$.studio.$errors" :key="error.$uid" class="error">
+            <strong>{{ error.$message }}</strong>
+          </p>
         </div>
         <button type="submit" class="btn btn-secondary">Create</button>
       </form>
@@ -43,6 +56,9 @@
 </template>
 
 <script>
+import useValidate from "@vuelidate/core";
+import { required, minLength, maxLength, alpha } from "@vuelidate/validators";
+
 import sideBar from "@/components/ui/side-bar.vue";
 import mainHeading from "@/components/ui/main-heading.vue";
 
@@ -53,6 +69,7 @@ export default {
   },
   data() {
     return {
+      v$: useValidate(),
       name: "",
       producer: "",
       studio: "",
@@ -60,22 +77,38 @@ export default {
   },
 
   created() {},
-
+  validations() {
+    return {
+      name: {
+        required,
+        minLength: minLength(4),
+        maxLength: maxLength(50),
+        alpha,
+      },
+      producer: { minLength: minLength(4), maxLength: maxLength(50), alpha },
+      studio: { required, minLength: minLength(4), maxLength: maxLength(50) },
+    };
+  },
   methods: {
     addNewClient() {
-      this.secured
-        .post("/clients", {
-          client: {
-            name: this.name,
-            studio: this.studio,
-            producer: this.producer,
-          },
-        })
-        .then((response) => {
-          console.log(response.data);
-          this.$router.replace("/clientsView");
-        })
-        .catch((error) => console.log(error, "Cannot create client"));
+      this.v$.$validate();
+      if (!this.v$.$error) {
+        this.secured
+          .post("/clients", {
+            client: {
+              name: this.name,
+              studio: this.studio,
+              producer: this.producer,
+            },
+          })
+          .then((response) => {
+            console.log(response.data);
+            this.$router.replace("/clientsView");
+          })
+          .catch((error) => console.log(error, "Cannot create client"));
+      } else {
+        console.log("Form cannot be validated");
+      }
     },
   },
   computed: {
